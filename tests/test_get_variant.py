c8d2eb03631f14811816e50fd4d4c1abcac738bf
moonso <monsunas@gmail.com>
2015-09-30 13:38:54 +0200

    Added test for get_variant

---
+++ tests/test_get_variant.py
@@ -0,0 +1,28 @@
+from mongomock import MongoClient
+from loqusdb.utils import get_variant
+
+def test_get_variant():
+    """Test to insert one variant"""
+    
+    client = MongoClient()
+    db = client['loqusdb']
+    
+    variant = {
+        'variant_id': 'test',
+    }
+    db.variant.insert(variant)
+    
+    mongo_variant = get_variant(db, variant)
+    assert mongo_variant['variant_id'] == 'test'
+
+def test_get_none():
+    """Test to get non existing variant"""
+
+    client = MongoClient()
+    db = client['loqusdb']
+
+    variant = {
+        'variant_id': 'test',
+    }
+
+    assert get_variant(db, variant) == None
