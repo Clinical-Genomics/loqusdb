f69e465b014c1460c842b00af470bdf37d730360
Robin Andeer <robin.andeer@gmail.com>
2016-02-29 15:39:39 +0100

    extract out logic from load CLI

--- loqusdb/commands/load_variants.py
+++ loqusdb/commands/load_variants.py
@@ -2,17 +2,17 @@ import os
 import logging
 import click
 
-from datetime import datetime
-
 from vcftoolbox import get_vcf_handle
 
 from loqusdb.exceptions import CaseError
 from loqusdb.vcf_tools import get_formated_variant
 from loqusdb.utils import get_family
+from loqusdb.utils import load_variants
 
 from . import base_command
 
 logger = logging.getLogger(__name__)
+
 
 @base_command.command()
 @click.argument('variant_file',
@@ -37,106 +37,34 @@ logger = logging.getLogger(__name__)
 @click.pass_context
 def load(ctx, variant_file, family_file, family_type, bulk_insert):
     """Load the variants of a case
-        
-        The loading is based on if the variant is seen in a ny affected individual
-        in the family.
+
+    The loading is based on if the variant is seen in a ny affected individual
+    in the family.
     """
     if not family_file:
         logger.error("Please provide a family file")
         ctx.abort()
-    
+
+    if variant_file != '-':
+        variant_file = os.path.abspath(variant_file)
+
     try:
-        family = get_family(
-            family_lines = family_file, 
-            family_type = family_type
-        )
-    except SyntaxError as err:
-        logger.warning(err.message)
-        ctx.abort()
-    
-    family_id = family.family_id
-    
-    affected_individuals = family.affected_individuals
-    
-    if not affected_individuals:
-        logger.error("No affected individuals could be found in ped file")
+        family = get_family(family_lines=family_file, family_type=family_type)
+    except SyntaxError as error:
+        logger.warning(error.message)
         ctx.abort()
 
-    logger.info("Found affected individuals in ped file: {0}".format(
-        ', '.join(affected_individuals)
-    ))
-    
-    adapter = ctx.obj['adapter']
-    
-    case = {
-        'case_id': family_id,
-        'vcf_path': os.path.abspath(variant_file)
-    }
-    
-    try:
-        adapter.add_case(case)
-    except CaseError as e:
-        logger.error(e.message)
+    if not family.affected_individuals:
+        logger.error("No affected individuals could be found in ped file")
         ctx.abort()
-    
-    if variant_file == '-':
-        logger.info("Start parsing variants from stdin")
-        variant_file = get_vcf_handle(
-            fsock=sys.stdin, 
-        )
-    else:
-        logger.info("Start parsing variants from stdin")
-        variant_file = get_vcf_handle(
-            infile=variant_file, 
-        )
-    
-    
-    #This is the header line with mandatory vcf fields
-    header = []
-    nr_of_variants = 0
-    nr_of_inserted = 0
-    
-    start_inserting = datetime.now()
-    start_ten_thousand = datetime.now()
-    
-    variants = []
-    for line in variant_file:
-        line = line.rstrip()
-        if line.startswith('#'):
-            if not line.startswith('##'):
-                header = line[1:].split()
-        else:
-            nr_of_variants += 1
-            
-            formated_variant = get_formated_variant(
-                variant_line = line,
-                header_line = header,
-                affected_individuals = affected_individuals
-            )
-            
-            if formated_variant:
-                nr_of_inserted += 1
-                if bulk_insert:
-                    variants.append(formated_variant)
-                else:
-                    adapter.add_variant(variant=formated_variant)
-            
-            if nr_of_variants % 10000 == 0:
-                logger.info("{0} of variants processed".format(nr_of_variants))
-                logger.info("Time to insert last 10000: {0}".format(
-                    datetime.now()-start_ten_thousand))
-                start_ten_thousand = datetime.now()
-                
-            if nr_of_variants % 100000 == 0:
-                if bulk_insert:
-                    adapter.add_bulk(variants)
-                    variants = []
-    
-    if bulk_insert:
-        adapter.add_bulk(variants)
-    
-    logger.info("Nr of variants in vcf: {0}".format(nr_of_variants))
-    logger.info("Nr of variants inserted: {0}".format(nr_of_inserted))
-    logger.info("Time to insert variants: {0}".format(datetime.now() - start_inserting))
-    
-    
+    logger.info("Found affected individuals in ped file: {0}"
+                .format(', '.join(family.affected_individuals)))
+
+    adapter = ctx.obj['adapter']
+    try:
+        load_variants(adapter, family.family_id, family.affected_individuals,
+                      variant_file, bulk_insert=bulk_insert,
+                      family_type=family_type)
+    except CaseError as error:
+        logger.error(error.message)
+        ctx.abort()
--- loqusdb/utils/__init__.py
+++ loqusdb/utils/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 
 from .get_families import get_family
+from .load import load_variants
