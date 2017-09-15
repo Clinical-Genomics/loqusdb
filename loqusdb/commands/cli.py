9a71541608b5dcbb3644a4d5831d0b944b7c7c5f
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-09-15 15:55:45 +0200

    Use cyvcf2 for loading and deleting. Fix #23, fix #25

--- loqusdb/commands/cli.py
+++ loqusdb/commands/cli.py
@@ -1,8 +1,14 @@
+import logging
 import click
+import coloredlogs
 
 from loqusdb.log import LEVELS, init_log
 from loqusdb import logger, __version__
 from loqusdb.plugins import MongoAdapter
+
+LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+
+logger = logging.getLogger(__name__)
 
 @click.group()
 @click.option('-db', '--database',
@@ -25,35 +31,25 @@ from loqusdb.plugins import MongoAdapter
                 show_default=True,
                 help='Specify the host where to look for the mongo database.'
 )
-@click.option('-b', '--backend',
-                default='mongo',
-                show_default=True,
-                type=click.Choice(['mongo',]),
-                help='Specify what backend to use.'
-)
+# @click.option('-b', '--backend',
+#                 default='mongo',
+#                 show_default=True,
+#                 type=click.Choice(['mongo',]),
+#                 help='Specify what backend to use.'
+# )
 @click.option('-c', '--conn_host',
                 default='mongodb://',
                 show_default=True,
                 help='Used for testing.'
 )
-@click.option('-l', '--logfile',
-                    type=click.Path(exists=False),
-                    help=u"Path to log file. If none logging is "\
-                          "printed to stderr."
-)
 @click.option('-v', '--verbose', count=True, default=1)
 @click.version_option(__version__)
 @click.pass_context
-def cli(ctx, conn_host, database, username, password, port, host, verbose, 
-        logfile, backend):
+def cli(ctx, conn_host, database, username, password, port, host, verbose):
     """loqusdb: manage a local variant count database."""
     # configure root logger to print to STDERR
     loglevel = LEVELS.get(max(verbose,1), "INFO")
-    init_log(
-        logger = logger, 
-        filename = logfile, 
-        loglevel = loglevel
-    )
+    coloredlogs.install(level=loglevel)
     
     # mongo uri looks like:
     #mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
