ef78f8a065b80d7fb5142bd836953424b82713a1
Mropat <mariaropat@gmail.com>
2021-11-26 14:56:53 +0100

    import

--- loqusdb/utils/load.py
+++ loqusdb/utils/load.py
@@ -10,13 +10,16 @@ This functions take an adapter which is the communication device for the databas
 import logging
 
 import click
-from loqusdb.build_models import build_case, build_profile_variant, build_variant
+
 from loqusdb.exceptions import CaseError, VcfError
 
 from .case import get_case, update_case
 from .delete import delete
 from .profiling import get_profiles, profile_match
 from .vcf import check_vcf, get_vcf
+from ..build_models.case import build_case
+from ..build_models.profile_variant import build_profile_variant
+from ..build_models.variant import build_variant
 
 LOG = logging.getLogger(__name__)
 
@@ -176,10 +179,8 @@ def load_case(adapter, case_obj, update=False):
         case_obj = update_case(case_obj, existing_case)
 
     # Add the case to database
-    try:
-        adapter.add_case(case_obj, update=update)
-    except CaseError as err:
-        raise err
+
+    adapter.add_case(case_obj, update=update)
 
     return case_obj
 
