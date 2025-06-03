fb5087b4018290ce4e94bde5355aaba44fa1aa57
ramprasadn <20065894+ramprasadn@users.noreply.github.com>
2025-06-03 12:02:48 +0200

    black

--- loqusdb/commands/export.py
+++ loqusdb/commands/export.py
@@ -58,8 +58,8 @@ def export(ctx, outfile, variant_type, freq):
                 existing_chromosomes.remove(chrom)
             elif genome == GRCH38:
                 if chrom[3:] in existing_chromosomes:
-                        ordered_chromosomes.append(chrom[3:])
-                        existing_chromosomes.remove(chrom[3:])
+                    ordered_chromosomes.append(chrom[3:])
+                    existing_chromosomes.remove(chrom[3:])
                 elif chrom in existing_chromosomes:
                     ordered_chromosomes.append(chrom)
                     existing_chromosomes.remove(chrom)
